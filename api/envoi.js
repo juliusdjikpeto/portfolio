8c9ccd02736cd4c2910733df216183419d6c640e
juliusdjikpeto <juliusdjikpeto544@gmail.com>
2026-04-04 16:16:22 +0100

    Update envoi.js

--- api/envoi.js
+++ api/envoi.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+    const { msg } = req.query;
+    const token = process.env.TELEGRAM_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
 
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${msg}`;
+    
+    try {
+        await fetch(url);
+        res.status(200).send("Sécurisé !");
+    } catch (error) {
+        res.status(500).send("Erreur");
+    }
+}
